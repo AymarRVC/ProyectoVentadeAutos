c3aa6f55c47e7e51bc312ca7f130c98e0974d286
Gabriel <gabrieles20012018@gmail.com>
2025-04-15 20:45:27 -0500

    Agregacion de la Conexion

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -4,10 +4,32 @@
  */
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author monta
+ * 
  */
 public class Conexion {
+     final String bdd="pure_witer"; 
+   final String user="root";
+    String pwd="";
+    String url="jdbc:mysql://localhost:3306/" + bdd;
+    Connection conexion;
     
+    public Connection getConexion(){
+    
+       try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           conexion=(Connection)DriverManager.getConnection(url,user,pwd);
+       } catch (ClassNotFoundException | SQLException ex) {
+           Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       
+        return conexion;
+    }
 }
